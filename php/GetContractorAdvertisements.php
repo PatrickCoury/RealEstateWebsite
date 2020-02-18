b4aeeacacb7ed7a7204de6d819574c7b03bfb20f
Tim Burtell <tburtell@emich.edu>
2020-02-17 21:58:28 -0500

    Adding GetContractorAdvertisements.php that returns a json object of a list with job objects containing the following fields: KEY NAME DESCRIPTION ADDRESS

---
+++ php/GetContractorAdvertisements.php
@@ -0,0 +1,37 @@
+<?php
+    
+    // Get SQL Connection object
+    $conn = $GLOBALS['SQL_CONN'];       
+
+    // Call query on SQL server
+    $query = 'select * from andrewsdreamllc.contractor;'
+    $result = mysqli_query($conn, $query);
+    
+    // If we have results 
+    if(mysqli_num_rows($result) > 0)
+    {
+        // Start packaging the query result into a json object 
+        $jobs = array();     // list 
+
+        while($row = mysqli_fetch_assoc($result)) 
+        {
+            // Each property will have a map of property info
+            $job= array();   
+            $job["KEY"] = $row["JOB_KEY"];
+            $job["NAME"] = $row["JOB_NAME"];
+            $job["DESCRIPTION"] = $row["JOB_DESCRIPTION"];
+            $job["ADDRESS"] = $row["JOB_ADDRESS"];
+
+            // Pack property into properties array 
+            array_push($job, $job);
+        }
+
+        // Print out the json object
+        echo json_encode($jobs);
+    }
+    else 
+    {
+        echo "Error: " . $conn . "<br>" . mysqli_error($conn);
+    }
+    
+?>
