544cfd2033cbfb36d99217cf70e0677f81706355
JacobEhrman <jrunehrman@gmail.com>
2020-03-18 18:15:43 -0400

    test

---
+++ javascript/about.js
@@ -0,0 +1,22 @@
+function getAbout()
+{
+    var request = new XMLHttpRequest();
+    request.open('GET', 'GetAboutUs.php', true);
+    request.onload = function()
+    {
+        var data = JSON.parse(this.response);
+        var description = data.About_us[0].DESCRIPTION;
+        var address = data.About_us[0].ADDRESS;
+        var contactNum = data.About_us[0].CONTACT_NUMBER;
+        var faxNum = data.About_us[0].FAX_NUMBER;
+        var email = data.About_us[0].EMAIL_ADDRESS;
+        var facbook = data.About_us[0].FACEBOOK;
+    }
+    request.send();
+}
+
+function populatePage(des, add, con, fax, ema, fac)
+{
+    var abt = documnet.getElementById('aboutDATA');
+    abt.createTextNode(des);
+}
